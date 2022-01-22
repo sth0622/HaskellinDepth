1eeb4fb8ce422e5347a7c707368a1335510e02c1
Taeho Son <sth0622@naver.com>
2022-01-22 20:40:18 +0900

    ch01 prototype

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,14 @@ module Lib
     ( someFunc
     ) where
 
+import Data.Char ( isLetter )
+import Data.List (group, sort)
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+    text <- TIO.readFile "./src/hamlet.txt"
+    let ws = map head $ group $ sort $ map T.toCaseFold $ filter (not . T.null) $ map (T.dropAround $ not . isLetter) $ T.words text
+    TIO.putStrLn $ T.unwords ws
+    print $ length ws
